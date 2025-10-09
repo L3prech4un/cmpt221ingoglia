0d51347e374a490a250135ed45632d5b4e6404e1
Matthew Ingoglia <Matthew.Ingoglia1@marist.edu>
2025-10-09 16:08:45 -0400

    Revert "Completed lab-5"
    This reverts commit 19abfee729822319e21a76aa08ff2bdd2903f885.

--- labs/lab-5/app.py
+++ labs/lab-5/app.py
@@ -18,17 +18,17 @@ def create_app():
     """Create Flask application and connect to your DB"""
     # create flask app
     app = Flask(__name__, 
-                template_folder=os.path.join(os.getcwd(), 'labs/lab-5/templates'), 
-                static_folder=os.path.join(os.getcwd(), 'labs/lab-5/static'))
+                template_folder=os.path.join(os.getcwd(), 'templates'), 
+                static_folder=os.path.join(os.getcwd(), 'static'))
     
     # connect to db
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url
+    # app.config["SQLALCHEMY_DATABASE_URI"] = db_url
     
     # Initialize database
-    with app.app_context():
-        if not init_database():
-            print("Failed to initialize database. Exiting.")
-            exit(1)
+    # with app.app_context():
+    #     if not init_database():
+    #         print("Failed to initialize database. Exiting.")
+            # exit(1)
 
     # ===============================================================
     # routes
@@ -41,15 +41,6 @@ def create_app():
         return render_template('index.html')
     
     # add more routes here!
-    @app.route('/brown.html')
-    def brown():
-        """Brown page"""
-        return render_template('brown.html')
-    
-    @app.route('/about.html')
-    def about():
-        """About page"""
-        return render_template('about.html')
 
     return app
 
